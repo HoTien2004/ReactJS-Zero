dac440b9a78ac7069671579b4fdcad72e3b19ad0
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2024-12-08 15:31:42 +0700

    1. Concept of Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const MyComponent = () => {
+  return (
+    <div>
+      <h2>MyComponent</h2>
+      <p>This is a custom component.</p>
+    </div>
+  );
+}
+
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
@@ -17,6 +26,7 @@ function App() {
         </a>
       </div>
       <h1>Hello World</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
