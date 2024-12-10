d9daf3a08fafff3f28fa8e179e6c1b92353c98ea
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2024-12-10 12:09:44 +0700

    3. JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,30 @@
+import './style.css';
+
 const MyComponent = () => {
+    const name = "TIENHO";
+    const age = 20;
+    const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const person = {
+        name: "TIENHO",
+        age: 25
+    }
     return (
-        <div>
-            <h2>MyComponent</h2>
-        </div>
+        // Fragment
+        <>
+            <div className="abc"
+                style={
+                    {
+                        borderRadius: "10px",
+                        backgroundColor: "yellow",
+                        color: "blue",
+                        padding: "20px",
+                        fontSize: "20px"
+                    }
+                }
+            >My Name: {person.name}, age: {age}, number: {JSON.stringify(number)}</div>
+            <div className='child'>MyComponent</div>
+            <div>{console.log("Hello world")}</div>
+        </>
     );
 }
 
